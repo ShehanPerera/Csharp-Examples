8a00696eb698e9957b5852c9c8edd432a47d6819
Shehan <12shehanakalanka@gmail.com>
2018-06-18 11:03:59 +0530

    Add New examples

---
+++ MyWebBrowser/MyWebBrowser/Mywebbrowser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyWebBrowser
+{
+    public partial class Mywebbrowser : Form
+    {
+        public Mywebbrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void prev_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoBack();
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            webBrowser.GoForward();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            webBrowser.Refresh();
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {          
+                webBrowser.Navigate(urlBox.Text);  
+        }
+
+        private void urlBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+               webBrowser.Navigate(urlBox.Text);
+            }
+        }   
+              
+    }
+}
