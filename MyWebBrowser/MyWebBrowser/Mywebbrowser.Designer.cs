eeba357f8519354fa9a208fd1646e9735736190f
Shehan <12shehanakalanka@gmail.com>
2018-07-04 12:48:23 +0530

    Revert "Revert "Add New examples""
    This reverts commit 82038c9b9c5764c41295e135daa2c4f046785619.

---
+++ MyWebBrowser/MyWebBrowser/Mywebbrowser.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MyWebBrowser
+{
+    partial class Mywebbrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.prev = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.Go = new System.Windows.Forms.Button();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.webBrowser, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.35955F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.64045F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(634, 356);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 6;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 421F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.prev, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.next, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.refresh, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Go, 4, 0);
+            this.tableLayoutPanel2.Controls.Add(this.urlBox, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(628, 37);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // prev
+            // 
+            this.prev.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.prev.Location = new System.Drawing.Point(3, 3);
+            this.prev.Name = "prev";
+            this.prev.Size = new System.Drawing.Size(32, 31);
+            this.prev.TabIndex = 0;
+            this.prev.Text = "<<";
+            this.prev.UseVisualStyleBackColor = true;
+            this.prev.Click += new System.EventHandler(this.prev_Click);
+            // 
+            // next
+            // 
+            this.next.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.next.Location = new System.Drawing.Point(41, 3);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(39, 31);
+            this.next.TabIndex = 1;
+            this.next.Text = ">>";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.refresh.Location = new System.Drawing.Point(86, 3);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(61, 31);
+            this.refresh.TabIndex = 2;
+            this.refresh.Text = "Refresh";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // Go
+            // 
+            this.Go.Location = new System.Drawing.Point(574, 3);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(40, 31);
+            this.Go.TabIndex = 3;
+            this.Go.Text = "Go";
+            this.Go.UseVisualStyleBackColor = true;
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // urlBox
+            // 
+            this.urlBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.urlBox.Location = new System.Drawing.Point(153, 3);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(415, 20);
+            this.urlBox.TabIndex = 4;
+            this.urlBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.urlBox_KeyPress);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(3, 46);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(628, 307);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // Mywebbrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 356);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Mywebbrowser";
+            this.Text = "MyWebBrowser";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button prev;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button Go;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
